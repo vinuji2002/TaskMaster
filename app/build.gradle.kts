8bbf9d7fb4889cbdc3183dda92da799bae26e570
Vinuji <vinujipalihakkara@gmail.com>
2024-05-10 23:01:41 +0530

    Fourth commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // room
+    def room_version = "2.3.0"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor ("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 
 }
 
