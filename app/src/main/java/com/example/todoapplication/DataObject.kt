9aec4be77d8e47ef6dcb059013ba20530a8d1cdf
Vinuji <vinujipalihakkara@gmail.com>
2024-05-10 22:40:59 +0530

    Second commit

--- app/src/main/java/com/example/todoapplication/DataObject.kt
+++ app/src/main/java/com/example/todoapplication/DataObject.kt
@@ -1,5 +1,18 @@
 package com.example.todoapplication
 
 object DataObject {
-    var listdata =
+    var listdata = mutableListOf<CardInfo>()
+
+    fun setData(title: String,priority:String){
+        listdata.add(CardInfo(title,priority))
+    }
+
+    fun getAllData(): List<CardInfo>
+    {
+        return listdata
+    }
+
+    fun deleteAll(){
+        listdata.clear()
+    }
 }
--- app/src/main/java/com/example/todoapplication/MainActivity.kt
+++ app/src/main/java/com/example/todoapplication/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.todoapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recycler_view.adapter=Adapter()
+        add.setOnClickListener {
+            val intent= Intent(this,CreateCard::class.java)
+            startActivity(intent)
+        }
+        deleteAll.setOnClickListener{
+            DataObject.deleteAll()
+        }
+
+        recycler_view.adapter=Adapter(DataObject.getAllData())
+        recycler_view.LayoutManager= LinearLayoutManager(this)
     }
 }
